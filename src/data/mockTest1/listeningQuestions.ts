1198047d071a60ca64eb8c2ec9975dc39b611622
jjtrainingcenter-lang <jjtrainingcenter@gmail.com>
2026-09-08 20:50:02 +0530

    feat: assemble Mock Test 1 listening questions

---
+++ src/data/mockTest1/listeningQuestions.ts
@@ -0,0 +1,12 @@
+import { Question } from '../../types';
+import { TEST1_LISTENING_PART_1 } from './listeningPart1';
+import { TEST1_LISTENING_PART_2 } from './listeningPart2';
+import { TEST1_LISTENING_PART_3 } from './listeningPart3';
+import { TEST1_LISTENING_PART_4 } from './listeningPart4';
+
+export const TEST1_LISTENING_QUESTIONS: Question[] = [
+  ...TEST1_LISTENING_PART_1,
+  ...TEST1_LISTENING_PART_2,
+  ...TEST1_LISTENING_PART_3,
+  ...TEST1_LISTENING_PART_4,
+];
